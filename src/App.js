65466d8629517cbea504f54781878d032e1063f8
John Lange <lange.john.m@gmail.com>
2020-11-30 15:04:28 -0500

    Basic Movie Listing from TMDB

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    this.loadMovies();
+  }
+
+  loadMovies() {
+    // go grab (aka get) all the data from some url
+    const url = "https://api.themoviedb.org/3/discover/movie?api_key=b6fbc7f3f313bd395902af464ef47262&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+    axios.get(url)
+      .then(response =>
+      {
+        // then put just the movies (not everything) into the state
+        this.setState({movies: response.data.results});
+      });
+
+  }
+
+  render() {
+    // let movies = [];
+    // for(let i = 0; i < this.state.movies.length; i++)
+    // {
+    //   movies.push(<h2>{this.state.movies[i].title}</h2>)
+    // }
+
+    return (
+      <div className="App">
+        <h1>Check out these movies!!!!!!</h1>
+        {this.state.movies.map(movie =>
+          {
+            return <h2>{movie.title} ({movie.release_date})</h2>;
+          })}
+      </div>
+    );
+  }
 }
 
 export default App;
